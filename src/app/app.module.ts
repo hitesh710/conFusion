32431382156cd0615e104be015812ca89a5d6da6
Hitesh Kumar <hitesh.kumar@accoliteindia.com>
2020-05-30 12:59:16 +0530

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
