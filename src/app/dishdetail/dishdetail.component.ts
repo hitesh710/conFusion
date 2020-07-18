eebbb7666811650eed895d26ea177ab960724f23
Hitesh Kumar <hitesh.kumar@accoliteindia.com>
2020-07-18 21:45:35 +0530

    Assignment 3.0

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { DishService } from '../services/dish.service';
+import { Comment } from '../shared/comment';
+
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -19,21 +23,88 @@ export class DishdetailComponent implements OnInit {
     prev: string;
     next: string;
 
+    commentForm: FormGroup;
+    comment: Comment;
+    @ViewChild('fform') commentFormDirective;
+
+    formErrors = {
+        'author': '',
+        'comment': '',
+    };
+
+    validationMessages = {
+        'author': {
+            'required': 'Name is required.',
+            'minlength': 'Name must be at least 2 characters long.'
+        },
+        'comment': {
+            'required': 'Comment is required.'
+        }
+    };
+
     constructor(private dishservice: DishService,
         private route: ActivatedRoute,
-        private location: Location) { }
+        private location: Location, private fb: FormBuilder) {
+        this.createForm();
+    }
 
     ngOnInit(): void {
         this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+        this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+            .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
     setPrevNext(dishId: string) {
-    const index = this.dishIds.indexOf(dishId);
-    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
-  }
+        const index = this.dishIds.indexOf(dishId);
+        this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+        this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    }
+
+    createForm() {
+        this.commentForm = this.fb.group({
+            author: ['', [Validators.required, Validators.minLength(2)]],
+            comment: ['', Validators.required],
+            rating: 1
+        });
+        this.commentForm.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged(); // (re)set validation messages now
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.commentForm) { return; }
+        const form = this.commentForm;
+        for (const field in this.formErrors) {
+            if (this.formErrors.hasOwnProperty(field)) {
+                // clear previous error message (if any)
+                this.formErrors[field] = '';
+                const control = form.get(field);
+                if (control && control.dirty && !control.valid) {
+                    const messages = this.validationMessages[field];
+                    for (const key in control.errors) {
+                        if (control.errors.hasOwnProperty(key)) {
+                            this.formErrors[field] += messages[key] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    onSubmit() {
+        this.comment = this.commentForm.value;
+        console.log(this.comment);
+        this.commentForm.reset({
+            author: '',
+            comment: '',
+            rating: 1
+
+        });
+        this.commentFormDirective.resetForm();
+    }
+
+
     goBack(): void {
         this.location.back();
-      }
+    }
 }
